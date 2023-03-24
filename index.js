8eb20d049379ce658aff928bdeb52e75e34ebdc0
Atik Ullah Khan <atikullah@gmail.com>
2023-03-24 12:18:47 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/callback", (req, res) => {
+  const { query, params, body } = req;
+
+  console.log({ query, params, body });
+
+  res.status(200).json({ message: "OK" });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`app listening on ${port}`);
+});
