b5c2c45fe392d92304613c13cf5346ba00b5663f
Atik Ullah Khan <atikullah.murad@gmail.com>
2025-09-22 06:52:54 +0000

    update webhook path and use secret from env

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+
+const pathaoWebhookSecret = process.env.PATHAO_WEBHOOK_SECRET;
+
+if (!pathaoWebhookSecret) {
+  console.error("❌ PATHAO_WEBHOOK_SECRET not found in environment variables.");
+  process.exit(1);
+}
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/callback", (req, res) => {
+app.use("/callback/pathao", (req, res) => {
   const { query, params, body } = req;
 
   const output = { query, params, body };
-
   console.log(JSON.stringify(output, null, 2));
 
-  res.status(200).json({ message: "OK" });
+  res
+    .status(202)
+    .set("X-Pathao-Merchant-Webhook-Integration-Secret", pathaoWebhookSecret)
+    .json({ message: "Accepted" });
 });
 
 const port = process.env.PORT || 3000;
